b2826a2ba01bbbe38065a9d7f9edb1a0ede025a0
luludev <523946878@qq.com>
2025-04-30 23:33:22 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import StoreProvider from '@/providers';
 import Header from '@/components/ui/Header';
 import Footer from '@/components/ui/Footer';
 import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script';
 
 
 export default function RootLayout({
@@ -12,6 +13,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="baidu-site-verification" content="codeva-oeTsHTdcjc" />
+        <meta
+          name="description"
+          content={"前端,全栈开发,Nodejs,react,angular,javascript,typescript,css,html,关于前端的一切"}
+        />
+        <meta
+          name="keywords"
+          content={"全栈开发,Nodejs,react,typescript,css,javascript,旅游，读书，见闻"}
+        />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/svg+xml" href="/images/logo.png" />
+        <link rel="sitemap" href="/sitemap-index.xml" />
+        <meta name="baidu_union_verify" content="7a742ef3ff07d7d9c0dd2e98e91ca2a1" />
+        <meta
+          name="google-site-verification"
+          content="SGbZh_3h9aOC3p6NGDz4XEe_aNk_5WTqybu4GCGVsK4"
+        />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9382467735244088"
+          crossOrigin="anonymous"></Script>
+      </head>
       <body>
         <StoreProvider>
           <div className="min-h-screen flex flex-col">
@@ -21,6 +46,7 @@ export default function RootLayout({
           </div>
         </StoreProvider>
         <Analytics />
+
       </body>
     </html>
   );
