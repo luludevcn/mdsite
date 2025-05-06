1eacb46d0c1c0546414d7777fb23bcfadebb7e35
luludev <523946878@qq.com>
2025-05-07 07:39:31 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         <a href="#" title=""><Image src={"/images/ads/ad04.png"} onClick={imageLoader} alt={"this is an ad."} fill className="object-cover" /></a>
       </div>
       <div className="w-3/5 mt-10 m-auto">
-        <Input type="text" placeholder="一切皆可查..." name="search" className="m-auto w-full pl-5 h-16 bg-amber-50 border border-gray-800 rounded-2xl data-focus:bg-blue-100 data-hover:shadow" />
+        <Input type="text" placeholder="一切皆可查..." name="search" className="m-auto w-full pl-5 h-16 text-gray-700 bg-amber-50 border border-gray-800 rounded-2xl data-focus:bg-blue-100 data-hover:shadow" />
       </div>
-      <div className="flex justify-start w-3/5 m-auto mt-5 gap-2 text-sm pl-5 text-gray-500">
+      <div className="flex justify-start w-3/5 m-auto mt-5 gap-2 text-sm pl-5 text-gray-400">
         <span>热搜词汇：</span><a href="">* Next</a><a href="">* React</a>
       </div>
     </div>
